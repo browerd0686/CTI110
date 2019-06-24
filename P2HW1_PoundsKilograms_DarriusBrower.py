2fb6d85ad014f27f75242b62fa7e465983076f69
browerd0686 <52142966+browerd0686@users.noreply.github.com>
2019-06-23 21:41:29 -0400

    Pounds to Kilograms first submission

---
+++ P2HW1_PoundsKilograms_DarriusBrower.py
@@ -0,0 +1,27 @@
+#A brief description of the project
+brief_description = 'Pounds to Kilograms Project'
+#Date
+date = 'June 18, 2019'
+#course
+course = 'CTI-110 P2HW1 - Pounds to Kilograms Converter'
+#your name
+first_name = 'Darrius'
+last_name = 'Brower'
+#
+print(brief_description)
+print(date)
+print(course)
+print(first_name, last_name)
+#
+#
+print('Input the lb')
+print('Get the results for the kg')
+print('Display the kg')
+#Calculate 
+kg = float(input('Enter the lb: '))
+lb = kg
+#Get the kg.
+kg = (lb/2.2046)
+
+#Display the kg.
+print('The kg is', kg)
