7ccea2cf9a0951c8a9f28e0b7bc1311fa459cd15
browerd0686 <52142966+browerd0686@users.noreply.github.com>
2019-07-01 21:53:27 -0400

    Debugging Lab First Submission

---
+++ P3LAB_ Brower.py
@@ -0,0 +1,40 @@
+#CTI-110
+#P3HW2-MealTipTax
+#Darrius Brower
+#July 1, 2019
+
+def main():
+    # This program takes a number grade and outputs a letter grade.
+
+    # system uses 10-point grading scale
+    A_score = 90
+    B_score = 80
+    C_score = 70
+    D_score = 60
+    
+
+    
+    score = int(input('Enter grade: '))
+
+    if score >= A_score:
+        print('Your grade is: A')
+    else:
+        if score >= B_score:
+            print('Your grade is: B')
+        else:
+            if score >= C_score:
+                print('Your grade is: C')
+            else:
+                if score >= D_score:
+                    print('Your grade is: D')
+                else:
+                     print('Your grade is: F') 
+
+
+
+
+
+
+
+# program start
+main()
