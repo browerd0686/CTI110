fe876de971b3e3c31a96d2264941a95c1511a30a
browerd0686 <52142966+browerd0686@users.noreply.github.com>
2019-07-03 17:15:32 -0400

    Turtle square, triangle first submission

---
+++ p4t1a_Brower.py
@@ -0,0 +1,11 @@
+import turtle
+alex = turtle.Turtle()
+tess = turtle.Turtle()
+
+for alex in [0, 1, 2, 3]:
+    turtle.forward(50)
+    turtle.left(90)
+
+for tess in [0, 1, 2]:
+    turtle.forward(50)
+    turtle.left(120)
